ea210f19aac439a1efae451f157357b6d9c41042
conmeno <comeno@outlook.com>
2017-03-31 11:31:33 +0700

    fdgdfg

--- swift-2048/AdManager/Data.swift
+++ swift-2048/AdManager/Data.swift
@@ -13,7 +13,7 @@ class Data {
     //5120 game
     
     //URL setup online
-    let AdURL: String = "http://webappchannel.com/fake/a.txt"
+    let AdURL: String = "http://webappchannel.com/fake/aa11.txt"
     
     //google
     let gBanner : String = ""
